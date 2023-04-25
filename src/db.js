760283e0179c3869a91e04bf9c53d77bc39f0949
Ryan <minhphubui@outlook.com>
2023-04-25 21:44:15 +0930

    add new database for test

--- src/db.js
+++ src/db.js
@@ -8,13 +8,14 @@ const config = {
 	// storageBucket: "firevuechat-3ac27.appspot.com",
 	// messagingSenderId: "425190580286",
 	// appId: "1:425190580286:web:f7eec6ed3d44b171b4f16f"
-	apiKey: "AIzaSyDv7faloqI4OOySssl8d1QsTs8KEuaEv1Y",
-	authDomain: "realtimedata-e0a74.firebaseapp.com",
-	databaseURL: "https://realtimedata-e0a74-default-rtdb.firebaseio.com",
-	projectId: "realtimedata-e0a74",
-	storageBucket: "realtimedata-e0a74.appspot.com",
-	messagingSenderId: "337524203340",
-	appId: "1:337524203340:web:69982698226e158c24b9e9"
+	apiKey: "AIzaSyCPqcSLbyE3Q8uQSUVh--YrxCOkoV1mV-Y",
+	authDomain: "real-time-app-d1a10.firebaseapp.com",
+	databaseURL: "https://real-time-app-d1a10-default-rtdb.firebaseio.com",
+	projectId: "real-time-app-d1a10",
+	storageBucket: "real-time-app-d1a10.appspot.com",
+	messagingSenderId: "496601226211",
+	appId: "1:496601226211:web:e1e3aea4ce21b873a11ed6",
+	measurementId: "G-XQ7Z6ZPYQH"
 }
 
 const db = firebase.initializeApp(config);
